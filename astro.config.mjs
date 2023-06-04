9ad4beab31d58fa42a2ed60bb093077e47cbad03
QuiiBz <tom.lienrd@gmail.com>
2023-06-04 16:45:17 +0200

    feat: use Lagon to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import lagon from '@lagon/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: lagon(),
 });
